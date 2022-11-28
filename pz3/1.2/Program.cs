a4b9706bc9a51b5d8efcf9079a3b01d4a3a66810
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-11-28 20:55:26 +0300

    2z

---
+++ pz3/1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿double Chet(int ax, int ay,int bx, int by)
+{
+    double result;
+    result = Math.Sqrt( Math.Pow(bx - ax,2) + Math.Pow(by - ay,2));
+    return result;
+}
+Console.WriteLine("первая к a");
+int ax = int.Parse(Console.ReadLine());
+Console.WriteLine("вторая к a");
+int ay = int.Parse(Console.ReadLine());
+Console.WriteLine("вторая к b");
+int bx = int.Parse(Console.ReadLine());
+Console.WriteLine("вторая к b");
+int by = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Round(Chet(ax,ay,bx,by)));
