d1c335e0afedc826f30a478f95868d0a436c8834
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-11-28 20:39:32 +0300

    0z

---
+++ pz3/1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿void Chet(int x, int y)
+{
+    if (x == 0 ||y  == 0)
+        Console.WriteLine("на оси");
+    else if (x > 0 && y  > 0)
+        Console.WriteLine("первая");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("четвертая");
+    else if (x < 0 && y  > 0)
+        Console.WriteLine("вторая");
+    else if (x < 0 && y  < 0)
+        Console.WriteLine("третья");
+}
+Console.WriteLine("первая к");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("вторая к");
+int y1 = int.Parse(Console.ReadLine());
+Chet(x1,y1);
